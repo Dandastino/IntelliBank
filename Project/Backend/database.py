0dd6e5c1b1afee0c51ece61d73dbf0b5ba53f297
dandastino <sherif.mustaa@gmail.com>
2025-04-09 12:08:17 +0200

    I have created the db and fix the pèroblem with the LICENSE.txt
    There are still some problem with database connectivity

--- Project/Backend/database.py
+++ Project/Backend/database.py
@@ -19,7 +19,30 @@ def save_transaction():
         print("Connected to database")
         print(os.getenv('POSTGRES_HOST'))
 
+        # Query per ottenere le tabelle
+        cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+        tables = cur.fetchall()
 
+        if tables:
+            print("Tables in the database:")
+            for table in tables:
+                print(table)
+        else:
+            print("No tables found in the database.")
+
+        # Se ci sono tabelle, eseguiamo la query per ottenere tutte le righe della tabella AppUser
+        if tables:
+            try:
+                cur.execute("SELECT * FROM AppUser;")
+                rows = cur.fetchall()
+                if rows:
+                    print("Rows in the AppUser table:")
+                    for row in rows:
+                        print(row)
+                else:
+                    print("No rows found in the AppUser table.")
+            except Exception as e:
+                print(f"Error fetching data from AppUser table: {e}")
     except OperationalError as e:
         print("Database connection failed:", e)
     except Exception as e:
